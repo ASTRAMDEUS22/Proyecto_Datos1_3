9c4d383e7bf30c4150835ad65b494c6719268a3a
josuect0212 <joscalvo@estudiantec.cr>
2023-05-30 23:40:17 -0600

    Update Main.java

--- Proyecto_3/src/main/java/Clases_auxiliares/Main.java
+++ Proyecto_3/src/main/java/Clases_auxiliares/Main.java
@@ -202,7 +202,7 @@ public class Main {
             }
 
             try {
-                Thread.sleep(2000);
+                Thread.sleep(1200);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
